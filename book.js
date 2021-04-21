d0ecb2ab552f89b80af04fd7d6949e5c43513cf8
spiralgalaxytrs <80592602+spiralgalaxytrs@users.noreply.github.com>
2021-04-21 10:48:07 +0530

    Update book.js

--- book.js
+++ book.js
@@ -1,3 +1,38 @@
+var url;
+function upload() {
+    //get your select image
+    var image=document.getElementById("Thumb").files[0];
+    //now get your image name
+    var imageName=image.name;
+    //firebase  storage reference
+    //it is the path where yyour image will store
+    var storageRef=firebase.storage().ref('images/'+imageName);
+    //upload image to selected storage reference
+
+    var uploadTask=storageRef.put(image);
+
+    uploadTask.on('state_changed',function (snapshot) {
+        //observe state change events such as progress , pause ,resume
+        //get task progress by including the number of bytes uploaded and total
+        //number of bytes
+        var progress=(snapshot.bytesTransferred/snapshot.totalBytes)*100;
+        console.log("upload is " + progress +" done");
+        document.getElementById("up").innerHTML=progress;
+    },function (error) {
+        //handle error here
+        console.log(error.message);
+    },function () {
+       //handle successful uploads on complete
+
+        uploadTask.snapshot.ref.getDownloadURL().then(function (downlaodURL) {
+            //get your upload image url here...
+            console.log(downlaodURL);
+            document.getElementById("url").innerHTML=downlaodURL;
+            url=downlaodURL;
+        });
+    });
+}
+
 function testVariable() {
     var strText1 = document.getElementById("name").value;
     var strText2 = document.getElementById("dob").value;
